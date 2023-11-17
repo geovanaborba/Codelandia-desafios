0ad0d8de9d448e5f0db6abb269105e918bfd1919
Geovana Borba <geborba19@hotmail.com>
2023-11-17 15:07:33 -0300

    Add Web Share API (compartilhamento nativo)

--- Desafio-7/assets/js/script.js
+++ Desafio-7/assets/js/script.js
@@ -15,3 +15,23 @@ const right = ScrollReveal({
     reset: false
 });
 ScrollReveal().reveal('.right', { delay: 200 });
+
+const share = document.querySelector('img.share');
+share.addEventListener('click', shareContent);
+
+/* ----------Botão de Compartilhar ---------- */ 
+function shareContent() {
+    if (navigator.share) {
+      navigator.share({
+        title: 'Desafio 7 - Valorant',
+        text: 'Confira a landing page de Valorant, criada por Geovana Borba para o desafio 7 da comunidade Codelândia no Discord.',
+        url: 'https://geovanaborba.github.io/Codelandia-desafios/Desafio-7/',
+      }).then(() => {
+        console.log('Compartilhado com sucesso!');
+      })
+      .catch(console.error);
+     } else {
+       // fallback
+       console.log('Seu sistema não suporta esse tipo de compartilhamento.');
+     }
+  };
