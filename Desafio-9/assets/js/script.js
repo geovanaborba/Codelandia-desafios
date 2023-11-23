dcb78f2c8038fb3e741fe037a5b64a3cf2985691
Geovana Borba <geborba19@hotmail.com>
2023-11-23 11:08:57 -0300

    efeito em texto e botão de voltar ao topo

---
+++ Desafio-9/assets/js/script.js
@@ -0,0 +1,20 @@
+var string = 'Olá, eu sou a Geovana Borba ♥'; 
+var array = string.split('');
+var timer;
+
+function frameLooper () {
+  if (array.length > 0) {
+    document.getElementById('text').innerHTML += array.shift();
+  } else {
+    clearTimeout(timer);
+      }
+  loopTimer = setTimeout('frameLooper()',120);
+
+}
+frameLooper();
+
+/* --- */
+var btn = document.querySelector('#back-to-top');
+btn.addEventListener('click', function() {
+    window.scrollTo(0, 0);
+});
