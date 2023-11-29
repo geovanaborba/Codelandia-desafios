72bde901bd0244d2c7f373c511bb0075648c93ac
Geovana Borba <geborba19@hotmail.com>
2023-11-29 20:32:08 -0300

    add função para ver certificados ao clicar

--- Desafio-9/assets/js/script.js
+++ Desafio-9/assets/js/script.js
@@ -1,20 +1,28 @@
-var string = 'Olá, eu sou a Geovana Borba ♥'; 
+var string = 'Olá, eu sou a Geovana Borba ♥';
 var array = string.split('');
 var timer;
 
-function frameLooper () {
+function frameLooper() {
   if (array.length > 0) {
     document.getElementById('text').innerHTML += array.shift();
   } else {
     clearTimeout(timer);
-      }
-  loopTimer = setTimeout('frameLooper()',120);
+  }
+  loopTimer = setTimeout('frameLooper()', 120);
 
 }
 frameLooper();
 
 /* --- */
 var btn = document.querySelector('#back-to-top');
-btn.addEventListener('click', function() {
-    window.scrollTo(0, 0);
+btn.addEventListener('click', function () {
+  window.scrollTo(0, 0);
+});
+
+/* --- */
+const elementsOpen = document.querySelectorAll('.open')
+elementsOpen.forEach(function (open) {
+  open.addEventListener('click', function () {
+    open.classList.toggle('ativa')
+  });
 });
