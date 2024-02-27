20598632330e55ec8ef2678d8c0e07b702ca06e1
Geovana Borba <geborba19@hotmail.com>
2024-02-27 13:27:25 -0300

    adiciona botões de next/prev ao carrossel de projetos

--- Desafio-9/assets/js/owl/setup.js
+++ Desafio-9/assets/js/owl/setup.js
@@ -1,6 +1,6 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
-    margin: 8,
+    margin: 10,
     center: true,
     startPosition: 1,
     autoplay: true,
@@ -10,14 +10,29 @@ $('.owl-carousel').owlCarousel({
         0:{
             items:1
         },
-        1000:{
+        1100:{
             items:1
         },
         1150:{
             items:2
         },
         1500:{
-            items:3
+            items:3,
+            margin: 70
         }
     }
+})
+
+/* -- Botões next/prev -- */
+var owl = $('.owl-carousel');
+owl.owlCarousel();
+// Go to the next item
+$('.customNextBtn').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+// Go to the previous item
+$('.customPrevBtn').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel', [300]);
 })
