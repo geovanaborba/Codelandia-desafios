59afbb265322f18ef4a3ec3e7c6a7b5e3e5f86a9
Geovana Borba <geborba19@hotmail.com>
2024-02-29 14:09:25 -0300

    adiciona animações de rolagem usando scrollReveal

---
+++ Desafio-9/assets/js/scrollReveal.js
@@ -0,0 +1,36 @@
+// left 
+const left = ScrollReveal({
+    origin: 'left',
+    distance: '880px',
+    duration: 2000,
+    reset: true,
+    delay: 200 
+});
+ScrollReveal().reveal('.describe p');
+ScrollReveal().reveal('.describe ul');
+ScrollReveal().reveal('.btn-describe');
+
+// right (feito com a classe diretamente, pois só usei uma)
+ScrollReveal().reveal('.photo', {
+    origin: 'right',
+    distance: '80px',
+    scale: 0.80,
+    duration: 2500,
+    reset: true,
+    delay: 200,
+});
+
+// bottom (mais de uma classe para os mesmos efeitos da const)
+const bottom = ScrollReveal({
+    origin: 'bottom',
+    distance: '50px',
+    duration: 2000,
+    reset: true,
+    delay: 200,
+    opacity: 0.4,
+    scale: 1,
+});
+ScrollReveal().reveal('.contacts-container');
+ScrollReveal().reveal('.certificates-container');
+ScrollReveal().reveal('.services-container');
+ScrollReveal().reveal('#skills i');
